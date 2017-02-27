0e217efc4684b4ba0f18eee23b86937a2c4bb402
yuguanghui <yuguanghui@lianfan.com>
2017-02-27 11:04:51 +0800

    改成

--- snake.py
+++ snake.py
@@ -2,6 +2,7 @@ import pygame, sys, random
 from pygame.locals import *
 
 # TODO 移动平滑，速度根据长度改变，随机出生地点，重构代码
+# TODO 这代码惨不忍睹，有屌大的帮我重构一下吗。没有的话我等会再问一遍
 
 #################################### 游戏基本设置
 WINDOW_WIDTH = 640
@@ -11,8 +12,15 @@ MOVE_SIZE = CELL_SIZE
 assert WINDOW_WIDTH % CELL_SIZE == 0 and WINDOW_HEIGHT % CELL_SIZE == 0, '窗口大小不合适'
 
 
-FPS = 10
+FPS = 14
 
+#################################### 颜色
+WHITE = (255,255,255)
+GREEN = (0,255,0)
+RED = (255,0,0)
+BLACK = (0,0,0)
+DARKGRAY  = ( 40,  40,  40)
+BGC = WHITE # 背景颜色
 
 #################################### 游戏逻辑
 SNAKE_LEN = 5 # 蛇的长度
@@ -24,25 +32,82 @@ SNAKE_UP = (0,-MOVE_SIZE)
 SNAKE_DOWN = (0,MOVE_SIZE)
 # 初始化方向
 SNAKE_DIR = SNAKE_RIGHT
+SNAKE_COLOR = RED
 
 APPLE = None
 
-
-
-
-#################################### 颜色
-WHITE = (255,255,255)
-GREEN = (0,255,0)
-RED = (255,0,0)
-BLACK = (0,0,0)
-DARKGRAY  = ( 40,  40,  40)
-BGC = WHITE # 背景颜色
-
-
 #################################### 离散变量，需要重构
-a = None
-b = None
 add = False
+
+
+
+class Snake():
+    def __init__(self, lens=SNAKE_LEN, direction=SNAKE_DIR, color=SNAKE_COLOR, cell_size=CELL_SIZE, move_size=MOVE_SIZE):
+        self.len = lens
+        self.direction = direction
+        self.color = color
+        self.cell_size = cell_size
+        self.move_size = move_size
+
+        self.nodes = []
+        self.dirs = []
+
+
+        for i in reversed(range(lens)):
+            self.nodes.append(pygame.Rect(i * cell_size, 0, cell_size, cell_size))
+            self.dirs.append([direction, direction])
+
+        self._tailNode = self.nodes[-1]
+
+
+    def add(self):
+        global add
+        self.len += 1
+        _tail_node = self._tailNode
+        _tail_dir = self.nodes[-1]
+        self.nodes.append(pygame.Rect(_tail_node.x, _tail_node.y, self.cell_size, self.cell_size))
+        self.dirs.append([_tail_dir[0], _tail_dir[1]])
+        add = False  # todo
+
+    def crash(self):
+        head = self.nodes[0]
+        for node in self.nodes[1:]: # 撞到自己
+            if node.colliderect(head):
+                return True
+        if head.left < 0:
+            return True
+        if head.right > WINDOW_WIDTH:
+            return True
+        if head.top < 0:
+            return True
+        if head.bottom > WINDOW_HEIGHT:
+            return True
+
+    # 吃到苹果
+    def eatApple(self, apple):
+        global add
+        if self.nodes[0].colliderect(apple):
+            add = True
+
+    def draw(self, SURFACE):
+        for node in self.nodes:
+            pygame.draw.rect(SURFACE, self.color, node)
+
+    def march(self):
+        self._tailNode = self.nodes[-1]
+
+        for i in range(len(self.dirs)):
+            self.dirs[i][0] = self.dirs[i][1]
+            if i == 0:
+                self.dirs[0][1] = self.direction
+            else:
+                self.dirs[i][1] = self.dirs[i - 1][0]
+            self.nodes[i] = self.nodes[i].move(self.dirs[i][1])
+
+
+
+
+
 
 
 class Apple(pygame.sprite.Sprite):
@@ -53,47 +118,7 @@ class Apple(pygame.sprite.Sprite):
 def genApple():
     return pygame.Rect(CELL_SIZE*random.randint(0,(WINDOW_WIDTH/CELL_SIZE)), CELL_SIZE*random.randint(0,(WINDOW_HEIGHT/CELL_SIZE)), CELL_SIZE, CELL_SIZE)
 
-# 初始化可爱的小蛇
-def initSnake():
-    global SNAKE_DIR, SNAKE_LEN
-    SNAKE_LEN = 5
-    SNAKE_DIR = SNAKE_RIGHT
-    nodes = []
-    dir = []
-    for i in reversed(range(SNAKE_LEN)):
-        nodes.append(pygame.Rect(i*CELL_SIZE,0,CELL_SIZE,CELL_SIZE))
-        dir.append([SNAKE_DIR,SNAKE_DIR])
-    return nodes,dir
 
-# 增加一格蛇的长度
-def snake_add(a,b):
-    global SNAKE_LEN, nodes, dirs, add
-    SNAKE_LEN += 1
-    nodes.append(pygame.Rect(a.x,a.y,CELL_SIZE,CELL_SIZE))
-    dirs.append([b[0],b[1]])
-    add = False
-
-# 吃到苹果
-def eatApple(rect):
-    if APPLE.colliderect(rect):
-        APPLE
-        return True
-    return False
-
-# 撞死
-def crash(nodes):
-    head = nodes[0]
-    for node in nodes[1:]: # 撞到自己
-        if node.colliderect(head):
-            return True
-    if head.left < 0:
-        return True
-    if head.right > WINDOW_WIDTH:
-        return True
-    if head.top < 0:
-        return True
-    if head.bottom > WINDOW_HEIGHT:
-        return True
 
 def drawPressKeyMsg():
     pressKeySurf = BASICFONT.render('Press a key to play.', True, DARKGRAY)
@@ -118,6 +143,7 @@ def showGameOverScreen():
     SURFACE.blit(overSurf, overRect)
     drawPressKeyMsg()
     pygame.display.update()
+    pygame.time.wait(500)
 
     while True:
         for event in pygame.event.get():
@@ -130,72 +156,74 @@ def showGameOverScreen():
                     # pygame.event.get()
                     return event.key
 
-nodes,dirs = initSnake()
-
-pygame.init()
-
-pygame.display.set_caption("snake")
-
-SURFACE = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-
-fpsClock = pygame.time.Clock()
-
-BASICFONT = pygame.font.Font('freesansbold.ttf', 18)
 
 
-while True:
+
+# 吃到苹果
+def eatApple(rect):
+    if APPLE.colliderect(rect):
+        return True
+    return False
+
+def snake_run(SURFACE):
+    global snake,APPLE,add
     SURFACE.fill(BGC)
 
-    for node in nodes:
-        pygame.draw.rect(SURFACE, RED, node)
+    snake.draw(SURFACE)
 
     if APPLE is None:
         APPLE = genApple()
     pygame.draw.rect(SURFACE, GREEN, APPLE)
 
-    if eatApple(nodes[0]):
+    if eatApple(snake.nodes[0]):
         add = True
-        a, b = nodes[-1], dirs[-1]
         APPLE = None
 
-    if crash(nodes):
+    if snake.crash():
         if showGameOverScreen():
-            nodes, dirs = initSnake()
-
+            snake = Snake()
 
     pygame.display.update()
 
+    snake.march()
+    if add:
+        snake.add()
+
+def eventProcessor():
     for event in pygame.event.get():
         if event.type == QUIT:
             terminate()
         if event.type == KEYDOWN:
             if event.key == K_LEFT:
-                if SNAKE_DIR is not SNAKE_RIGHT:
-                    SNAKE_DIR = SNAKE_LEFT
+                if snake.direction is not SNAKE_RIGHT:
+                    snake.direction = SNAKE_LEFT
             if event.key == K_RIGHT:
-                if SNAKE_DIR is not SNAKE_LEFT:
-                    SNAKE_DIR = SNAKE_RIGHT
+                if snake.direction is not SNAKE_LEFT:
+                    snake.direction = SNAKE_RIGHT
             if event.key == K_UP:
-                if SNAKE_DIR is not SNAKE_DOWN:
-                    SNAKE_DIR = SNAKE_UP
+                if snake.direction is not SNAKE_DOWN:
+                    snake.direction = SNAKE_UP
             if event.key == K_DOWN:
-                if SNAKE_DIR is not SNAKE_UP:
-                    SNAKE_DIR = SNAKE_DOWN
+                if snake.direction is not SNAKE_UP:
+                    snake.direction = SNAKE_DOWN
+
+def mainLoop():
+    while True:
+        eventProcessor()
+        snake_run(SURFACE)
+        fpsClock.tick(FPS)
 
 
-    for i in range(len(dirs)):
-        dirs[i][0] = dirs[i][1]
-        if i==0:
-            dirs[0][1] = SNAKE_DIR
-        else:
-            dirs[i][1] = dirs[i-1][0]
-        nodes[i] = nodes[i].move(dirs[i][1])
+if __name__ == '__main__':
+    # 初始化贪吃蛇
+    pygame.init()
+    pygame.display.set_caption("snake")
+    SURFACE = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    fpsClock = pygame.time.Clock()
+    BASICFONT = pygame.font.Font('freesansbold.ttf', 18)
 
-    if add:
-        snake_add(a,b)
-
-    fpsClock.tick(FPS)
-
+    snake = Snake()
+    mainLoop()
 
 
 
